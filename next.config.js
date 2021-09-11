2a14d803b6e33af29ed7f3ae85a6653e5a53601c
amir9009 <amirhoseinnemati79@gmail.com>
2021-09-11 17:44:13 +0430

    a

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     // BASE_URL: "http://localhost:3000",
-    // BASE_URL: "http://127.0.0.1:3000",
     BASE_URL: "https://store-oa76moq85-elieliot9009-gmailcom.vercel.app",
-    // BASE_URL: "https://store-lemon.vercel.app",
     MOBGODB_URL:
       "mongodb+srv://eli:elieli@cluster0.stk42.mongodb.net/store?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "YWJ}>q%b*>V4.&pj4}6@q>jre+2-sKJe>p_M);Z~zsVVn-JsTP",
